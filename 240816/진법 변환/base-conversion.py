acb2a23f84bf84329f8a90d1c386f5b5db730aca
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-08-16 17:25:01 +0900

    Title: 진법 변환, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240816/진법 변환/base-conversion.py
+++ 240816/진법 변환/base-conversion.py
@@ -10,8 +10,8 @@ len_n = len(n)
 # Unicode 48~57: 0~9 숫자
 #         65~90: A~Z
 
-for i in range(len_n-1, -1, -1):
-    num = ord(n[i])
+for i in range(0, len_n):
+    num = ord(n[len_n-i-1])
     if num in range(48, 58):
         num -= 48
     else:
