2212fdd1349b0f8782b6e622b2578a340d2f1d4e
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-08-16 11:30:33 +0100

    Title: 문자 대치, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240816/문자 대치/character-replacement.py
+++ 240816/문자 대치/character-replacement.py
@@ -17,7 +17,7 @@ for i in range(len_s):
     c = s[i]
     if c == '.':
         dp[i] = dp[i-1] + '.'
-        tmp = 0
+        tmp = 0   # 이걸 안 해서 틀림!!!
         continue
     if c == 'X':
         tmp+=1
