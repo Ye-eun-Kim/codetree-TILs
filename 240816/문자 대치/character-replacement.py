8a2f3ba04011a045b8ea4ac5178493ce713832fe
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-08-16 11:17:22 +0100

    Title: 문자 대치, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240816/문자 대치/character-replacement.py
@@ -0,0 +1,32 @@
+s = input()
+len_s = len(s)
+dp = ['']*(len_s)
+
+
+tmp = 0
+for i in range(len_s):
+    c = s[i]
+    if c == '.':
+        dp[i] = dp[i-1] + '.'
+        continue
+    if c == 'X':
+        tmp+=1
+    if tmp == 4:
+        dp[i] = dp[i-4]+'aaaa'
+        tmp = 0
+    elif tmp == 2:
+        if i == 1:
+            dp[i] = 'bb'
+            continue
+        now = dp[i-2]+'bb'
+        org = dp[i]
+        if org == '':
+            dp[i] = now
+        else:
+            if org == dp[i-2]+'aa':
+                continue
+
+if dp[-1] == '':
+    print(-1)
+else:
+    print(dp[-1])
