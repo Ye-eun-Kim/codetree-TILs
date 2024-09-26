042b14e78e7ca52042f6c0e12e394e8dd03ebe94
김예은 <63407818+Ye-eun-Kim@users.noreply.github.com>
2024-09-26 22:13:20 +0100

    Title: n x n 표 채우기, Time: 202ms, Memory: 30MB, Status: Passed - CodeTree

--- 240927/n x n 표 채우기/populate-an-n-x-n-table.py
+++ 240927/n x n 표 채우기/populate-an-n-x-n-table.py
@@ -10,6 +10,7 @@ num = 1
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 d = 0
+x, y = 0, 0
 
 # 이전 숫자 +1을 입력
 # 어디에?
@@ -22,6 +23,8 @@ for i in range(n):
             break
         for _ in range(i+1):
             graph[r][c] = num
+            if m == num:
+                x, y = r, c
             num += 1
             if num == n**2+1:
                 stop = True
@@ -35,3 +38,4 @@ for k in range(n):
     for j in range(n):
         print(graph[k][j], end=" ")
     print()
+print(x+1, y+1)
